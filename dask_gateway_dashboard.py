836347a5887ac52363798b4f4581609bd6f37d98
Min RK <benjaminrk@gmail.com>
2025-02-17 09:42:05 +0100

    add memory, layout to dashboard

--- dask_gateway_dashboard.py
+++ dask_gateway_dashboard.py
@@ -1,16 +1,18 @@
 import logging
+import os
+from dataclasses import dataclass
 from pathlib import Path
 from typing import TypedDict
 
+from dask.utils import format_bytes
 from dask_gateway import Gateway
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 
 app_dir = Path(__file__).parent.resolve()
 index_html = app_dir / "index.html"
-app = FastAPI()
-
 log = logging.getLogger("uvicorn.error")
+app = FastAPI()
 
 
 class ClusterModel(TypedDict):
@@ -18,18 +20,22 @@ class ClusterModel(TypedDict):
     dashboard_link: str
     workers: int
     cores: float
+    memory: str
 
 
 def make_cluster_model(cluster) -> ClusterModel:
     """Make a single cluster model"""
+    # derived from dask-labextension Manager
     info = cluster.scheduler_info
     cores = sum(d["nthreads"] for d in info["workers"].values())
     workers = len(info["workers"])
+    memory = format_bytes(sum(d["memory_limit"] for d in info["workers"].values()))
     return {
         "name": cluster.name,
         "dashboard_link": cluster.dashboard_link,
         "workers": workers,
         "cores": cores,
+        "memory": memory,
     }
 
 
@@ -45,13 +51,53 @@ async def list_clusters() -> list[ClusterModel]:
     return clusters
 
 
+@dataclass
+class _MockCluster:
+    """Mock cluster for UI development"""
+
+    name: str
+    workers: int
+    cores_per_worker: int = 2
+
+    @property
+    def dashboard_link(self) -> str:
+        return f"http://localhost:8000/cluster/{self.name}"
+
+    @property
+    def scheduler_info(self) -> dict:
+        return {
+            "workers": {
+                f"id-{n}": {
+                    "nthreads": self.cores_per_worker,
+                    "memory_limit": 1_000_000_000,
+                }
+                for n in range(self.workers)
+            }
+        }
+
+
+async def _mock_list_clusters() -> list[ClusterModel]:
+    """mock cluster list for UI development"""
+    clusters = []
+    for i in range(3):
+        cluster = _MockCluster(workers=i, name=f"cluster {i}")
+        clusters.append(make_cluster_model(cluster))
+    return clusters
+
+
+if os.environ.get("_GATEWAY_MOCK_CLUSTERS"):
+    list_clusters = _mock_list_clusters
+
+
 @app.get("/")
 async def get():
+    """serve the HTML page"""
     with index_html.open() as f:
         return HTMLResponse(f.read())
 
 
 @app.get("/clusters")
 async def get_clusters():
+    """Return list of clusters as JSON"""
     clusters = await list_clusters()
     return JSONResponse(clusters)
